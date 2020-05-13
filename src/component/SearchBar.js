a1f2d539f020009622e9d1e1b1d41c17a16157c9
Md Riyaz Kazmi <mdriyazansari.18mca@kongu.edu>
2020-05-13 15:07:55 +0000

    Initial commit

---
+++ src/component/SearchBar.js
@@ -0,0 +1,29 @@
+import React from "react"
+import VideoStyle from "./VideoStyle"
+export default class SearchBar extends React.Component{
+  state={term:''}
+  onInputChange=(event)=>{
+    this.setState({term:event.target.value})
+  }
+  onFormSubmit=(event)=>{
+    event.preventDefault();
+  }
+  render(){
+    return(
+      <div>
+      <VideoStyle/>
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="form">
+           <input type="text"
+           value={this.state.term}
+            placeholder="Search..."
+            onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+      </div>
+    )
+  }
+}
